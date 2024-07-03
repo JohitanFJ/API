771740f3b279700fd637da84f6dddc4cace26b95
Ethan7FJ <johanfetecua11@gmail.com>
2024-07-02 23:26:07 -0500

    API2/07/2024

---
+++ cinfig/database.js
@@ -0,0 +1,9 @@
+const dab = {
+    host: "roundhouse.proxy.rlwy.net",
+    user: "root",
+    password: 'DByztHOKWbbSmkDnkCFwOHRrlWBMLFgm',
+    database: 'railway',
+    port: 42476,
+}
+
+export default dab
